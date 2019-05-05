d38247554cb0839dbfa650c00779cc186e1e3da5
AlexeyCorr <alexeycorr@gmail.com>
2019-05-05 23:30:38 +0300

    Добавляет тесты для компонентов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,17 +3,19 @@ import WelcomeScreen from './../welcome-screen/welcome-screen.jsx';
 import PropTypes from 'prop-types';
 
 const App = (props) => {
-  const {gameTime, errorCount} = props;
+  const {gameTime, errorCount, onClickButtonStart} = props;
 
   return <WelcomeScreen
     time={gameTime}
     errorCount={errorCount}
+    onClickButtonStart={onClickButtonStart}
   />;
 };
 
 App.propTypes = {
   gameTime: PropTypes.number.isRequired,
-  errorCount: PropTypes.number.isRequired
+  errorCount: PropTypes.number.isRequired,
+  onClickButtonStart: PropTypes.func.isRequired
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ const init = () => {
   const settings = {
     gameTime: 7,
     errorCount: 4,
+    onClickButtonStart() {
+      return 1;
+    }
   };
 
   ReactDOM.render(
       <App
         errorCount={settings.errorCount}
         gameTime={settings.gameTime}
+        onClickButtonStart={settings.onClickButtonStart}
       />,
       document.querySelector(`.main`)
   );
