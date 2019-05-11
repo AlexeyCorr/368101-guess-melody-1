597b80957a91e447244b60f611b58ca18791905c
AlexeyCorr <alexeycorr@gmail.com>
2019-05-12 00:43:59 +0300

    Исправляет ошибки в пропсах

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -2,12 +2,41 @@ import React from 'react';
 import App from './app.jsx';
 import renderer from 'react-test-renderer';
 
+const mock = {
+  questions: [
+    {
+      type: `genre`,
+      genre: `rock`,
+      answers: [
+        {
+          src: `path`,
+          genre: `rock`,
+        },
+      ],
+    },
+    {
+      type: `artist`,
+      song: {
+        artist: `One`,
+        src: ``,
+      },
+      answers: [
+        {
+          picture: ``,
+          artist: `One`,
+        },
+      ],
+    }
+  ],
+};
+
 it(`renders correctly`, () => {
+  const {questions} = mock;
   const tree = renderer
     .create(<App
       gameTime={0}
       errorCount={0}
-      onClickButtonStart={jest.fn()}
+      questions={questions}
     />)
     .toJSON();
   expect(tree).toMatchSnapshot();
